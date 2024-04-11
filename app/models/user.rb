83ba94acb2d798d4ad979f9956e68e91dd8bc277
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-04-11 14:50:26 +0900

    usersモデルファイルに必要なアソシエーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :furimas
+  has_many :purchases
+
   validates :nickname, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
