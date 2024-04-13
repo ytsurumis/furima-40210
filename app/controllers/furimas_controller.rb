a885fd307fac32baff7179e918222e5376ebe395
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-04-13 14:27:08 +0900

    画像表示の実装

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,4 +1,15 @@
 class FurimasController < ApplicationController
   def index
+    @furimas = Furima.all
+  end
+
+  def new
+    @furima = Furima.new
+  end
+
+  private
+
+  def furima_params
+    params.require(:furima).permit(:image).merge(user_id: current_user.id)
   end
 end
--- app/models/furima.rb
+++ app/models/furima.rb
@@ -1,3 +1,4 @@
 class Furima < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 end
