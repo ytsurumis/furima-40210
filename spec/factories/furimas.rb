42f89ab9d608059e43c99e74b33d3bdf36cab5ce
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-04-17 19:56:11 +0900

    コードの修正

--- spec/factories/furimas.rb
+++ spec/factories/furimas.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :furima do
     title                  { Faker::Name.initials(number: 2) }
     description            { Faker::Lorem.sentence }
-    category_id            { Faker::Number.number(digits: rand(1..2)) }
-    condition_id           { Faker::Number.number(digits: rand(1..2)) }
-    burden_id              { Faker::Number.number(digits: rand(1..2)) }
-    area_id                { Faker::Number.number(digits: rand(1..2)) }
-    number_of_day_id       { Faker::Number.number(digits: rand(1..2)) }
+    category_id            { Faker::Number.between(from: 2, to: 11) }
+    condition_id           { Faker::Number.between(from: 2, to: 7) }
+    burden_id              { Faker::Number.between(from: 2, to: 3) }
+    area_id                { Faker::Number.between(from: 2, to: 48) }
+    number_of_day_id       { Faker::Number.between(from: 2, to: 4) }
     price                  { Faker::Commerce.price(range: 300..9_999_999, as_string: true) }
     after(:build) do |object|
       object.image.attach(io: File.open('spec/factories/test.jpg'), filename: 'test.jpg', content_type: 'image/jpeg')
