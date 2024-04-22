149508d315ddb130b8311030e53c57e50803c796
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-04-22 20:37:12 +0900

    不要な記述の削除

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -2,7 +2,6 @@ class FurimasController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @furimas = Furima.all
     @furimas = Furima.order(created_at: :desc)
   end
 
