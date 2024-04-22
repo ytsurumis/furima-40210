45dcedd4bff08f3e037e86536c1ba1b7a1484eef
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-04-22 19:47:42 +0900

    商品一覧機能の実装

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -2,7 +2,7 @@ class FurimasController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    # @furimas = Furima.all
+    @furimas = Furima.all
   end
 
   def new
