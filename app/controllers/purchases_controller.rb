6f578228eee635dfd19be14837755a9c5de9607b
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-05-03 14:45:08 +0900

    ビューファイルの修正
    配送情報が入力できるように修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,5 +6,18 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase = Purchase.create(purchase_params)
+    Shipping.create(shipping_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:user_id, :furima_id)
+  end
+
+  def shipping_params
+    params.permit(:post_code, :area_id, :municipalities, :streer, :buinding, :phone).merge(purchase_id: @purchase.id)
   end
 end
