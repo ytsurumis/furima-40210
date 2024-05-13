7ea86b8ecfd945b895ac4b86b89141815bdef519
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-05-13 15:39:43 +0900

    同じ内容の記述を一つにまとめる

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -7,14 +7,12 @@ class PurchaseShipping
     validates :furima_id
     validates :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-  end
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
-  with_options presence: true do
     validates :municipalities
     validates :street
     validates :phone, length: { in: 10..11, message: 'is too short' }
   end
   validates :phone, numericality: { message: 'is not a number' }
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     purchase = Purchase.create(user_id:, furima_id:)
